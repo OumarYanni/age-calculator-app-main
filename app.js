6b5cda72630f7c79dda09cffc040e2545dbb5756
Oumar Yanni <oumaryanni@gmail.com>
2023-12-08 16:57:02 +0000

    Update form validation

--- app.js
+++ app.js
@@ -13,40 +13,49 @@ form.addEventListener("submit", function (e) {
 // Objectif : avoir un truc qui marche mais si c'est moche
 
 function isInputValid() {
-    const dayInputElement = document.querySelector("#day-input");
-    const dayInputValue = dayInputElement.value;
-    const isDayValid = dayInputValue >= 1 && dayInputValue <= 31
-    
-    const monthInputElement = document.querySelector("#month-input");
-    const monthInputValue = monthInputElement.value;
-    
-    const yearInputElement = document.querySelector("#year-input");
-    const yearInputValue = yearInputElement.value;
+  const dayInputElement = document.querySelector("#day-input");
+  const dayInputValue = dayInputElement.value;
+  const isDayValid = dayInputValue >= 1 && dayInputValue <= 31;
 
-    console.log(isDayValid)
+  const monthInputElement = document.querySelector("#month-input");
+  const monthInputValue = monthInputElement.value;
+  const isMonthValid = monthInputValue >= 1 && monthInputValue <= 12;
 
-    // Ce n'est pas valide
-    if (!dayInputValue || !monthInputValue || !yearInputValue) {
-        console.log("===")
-    } else { // C'est valide
+  const yearInputElement = document.querySelector("#year-input");
+  const yearInputValue = yearInputElement.value;
 
-    }
+  let currentDate = new Date();
+  let dateToBeVerified = new Date(
+    yearInputValue,
+    monthInputValue,
+    dayInputValue
+  );
+  const isDateInTheFuture = dateToBeVerified < currentDate;
 
+  console.log(isDayValid);
+  console.log(isMonthValid);
+  console.log(isDateInTheFuture);
 
+  // Ce n'est pas valide
+  if (!dayInputValue || !monthInputValue || !yearInputValue) {
+    console.log("===");
+  } else {
+    // C'est valide
+  }
 
-//   let dayErrorElement = document.querySelector("#day-error");
-//   dayErrorElement.textContent = "";
+  //   let dayErrorElement = document.querySelector("#day-error");
+  //   dayErrorElement.textContent = "";
 
-//   let isValid = true;
-//   const dayInputElement = document.querySelector("#day-input");
-//   const dayInputValue = dayInputElement.value;
+  //   let isValid = true;
+  //   const dayInputElement = document.querySelector("#day-input");
+  //   const dayInputValue = dayInputElement.value;
 
-//   if (!dayInputValue) {
-//     dayErrorElement.textContent = "this field is required";
-//     isValid = false;
-//   }
+  //   if (!dayInputValue) {
+  //     dayErrorElement.textContent = "this field is required";
+  //     isValid = false;
+  //   }
 
-//   return isValid;
+  //   return isValid;
 }
 
 // let dayErrorElement = document.querySelector("#day-error");
